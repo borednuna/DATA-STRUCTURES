b7e67a29d407acf746c3ab1770f84cfd910f649c
hanunshaka <hanunshaka02@gmail.com>
2022-03-08 11:10:05 +0700

    feat: add palindrome and anagram check

---
+++ Lectures/Anagram.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    char alph[26] = {0};
+
+    int check = 1;
+    printf("Kata 1: "); scanf("%s", str1);
+    printf("Kata 2: "); scanf("%s", str2);
+    printf("%s %s\n", str1, str2);
+
+    char *first, *sec;
+    first = &str1[0];
+    sec = &str2[0];
+
+    if (strlen(str1) != strlen(str2)) {
+        check = 0;
+    }
+
+    while (*first != '\0' && *sec != '\0') {
+        alph[*first - 'a']++;
+        alph[*sec - 'a']--;
+        first++; sec++;
+    }
+
+    for (int i=0; i < 26; i++) {
+        if (alph[i] != 0) {
+            check = 0;
+            break;
+        }
+    }
+
+    if (check) {
+        printf("ANAGRAM\n");
+    } else {
+        printf("BUKAN ANAGRAM\n");
+    }
+}
