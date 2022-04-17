35f62d7023d74171331aca7c5f95c43c6e39b8ee
hanunshaka <hanunshaka02@gmail.com>
2022-04-18 04:39:09 +0700

    draft: continuing separateChaining.c

--- Lectures/separateChaining.c
+++ Lectures/separateChaining.c
@@ -14,3 +14,50 @@ struct arrayData {
 struct arrayData *array;
 int size = 0;
 int max = 7;
+
+int hashIndex(int key) {
+    return (key % max);
+}
+
+
+
+struct node* getElement(struct node *list, int findIndex) {
+    int i = 0;
+    struct node *temp = list;
+
+    while (i != findIndex) {
+        temp = temp->next;
+        i++;
+    }
+    return temp;
+}
+
+void initArray() {
+    for (int i=0; i < max; i++) {
+        array[i].head = NULL;
+    }
+}
+
+int find(struct node *list, int key) {
+    int returnValue = 0;
+}
+
+void insertData (int key, int value) {
+    float loadFactor = 0.0;
+
+    int index = hashIndex(key);
+
+    struct node *list = (struct node*) array[index].head;
+    struct node *newItem = (struct node*) malloc(sizeof(struct node));
+    newItem->key = key;
+    newItem->value = value;
+    newItem->next = NULL;
+
+    if (list == NULL) {
+        printf("Data key %d berhasil ditambahkan.\n\n", key);
+        array[index].head = newItem;
+        size++;
+    } else {
+        
+    }
+}
